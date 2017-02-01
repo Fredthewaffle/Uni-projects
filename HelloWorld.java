87cd0083d7c21027a00e83e8478d9a02b083ece2
Fredthewaffle <dadb@itu.dk>
2017-02-01 10:58:50 +0100

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by blizz on 01/02/2017.
+ */
+import java.io.PrintStream;
+public class HelloWorld {
+    public static void main(String[] args) {
+        PrintStream out = System.out;
+        out.println("Hello World!");
+    }
+}
